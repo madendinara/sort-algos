0e19d5c8c85b546f5124c62ef9dbd5d72d13ed16
Dinara Zimanova <madendinara@gmail.com>
2022-04-27 15:56:44 +0600

    Insertion Sort added

--- src/kz/javabegin/algos/InsertionSort.java
+++ src/kz/javabegin/algos/InsertionSort.java
@@ -2,4 +2,22 @@ package kz.javabegin.algos;
 
 public class InsertionSort {
 
+    public static void insertion() {
+        int[] arr = {20, 35, -5, 3, 45, -22, 1};
+
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int current = arr[firstUnsortedIndex];
+            int lastSortedIndex = firstUnsortedIndex - 1;
+
+            while (lastSortedIndex >= 0 && current < arr[lastSortedIndex]){
+                arr[lastSortedIndex + 1] = arr[lastSortedIndex];
+                lastSortedIndex--;
+            }
+            arr[lastSortedIndex + 1] = current;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
--- src/kz/javabegin/algos/Main.java
+++ src/kz/javabegin/algos/Main.java
@@ -3,6 +3,8 @@ package kz.javabegin.algos;
 public class Main {
 
     public static void main(String[] args) {
-        new BubbleSort().bubble();
+        //new BubbleSort().bubble();
+
+        new InsertionSort().insertion();
     }
 }
