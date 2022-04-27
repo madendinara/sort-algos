16e236ff30e0165c55bd6c201d8980e8fe98a732
Dinara Zimanova <madendinara@gmail.com>
2022-04-27 16:28:02 +0600

    Selection Sort added

--- src/kz/javabegin/algos/Main.java
+++ src/kz/javabegin/algos/Main.java
@@ -3,8 +3,6 @@ package kz.javabegin.algos;
 public class Main {
 
     public static void main(String[] args) {
-        //new BubbleSort().bubble();
-
-        new InsertionSort().insertion();
+        new SelectionSort().selection();
     }
 }
---
+++ src/kz/javabegin/algos/SelectionSort.java
@@ -0,0 +1,30 @@
+package kz.javabegin.algos;
+
+public class SelectionSort {
+
+    public static void selection() {
+
+        int[] arr = {20, 35, -5, 3, 45, -22, 1};
+
+        for(int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int minId = i;
+
+            for(int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minId = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = min;
+            arr[minId] = temp;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
