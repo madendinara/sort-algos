d1456a773da38883f9a0e85edc928233e1467e9a
Dinara Zimanova <madendinara@gmail.com>
2022-07-08 23:05:46 +0600

    fixes

--- src/kz/javabegin/algos/Main.java
+++ src/kz/javabegin/algos/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {34, 3, 5, -4, 12, 4, -9, 1};
-        Example.insertionSort(arr);
 //        int index = BinarySearch.binary(arr, 12, 0, arr.length - 1);
 //        System.out.println(index);
         for(int i = 0; i < arr.length; i++) {
